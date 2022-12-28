f0f0c41375b6d5483c7cc9773d7483fd3f5169e5
BrunodevBandeira <brunodev29ab@gmail.com>
2022-12-27 22:56:56 -0300

    WIP LandingPAge

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className={styles.footer}>
+      Footer...
+    </section>
   )
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import styles from "./Main.module.css";
 import foto from "../../assets/Hero.svg";
+import iconEllipse from "../../assets/iconEllipse.svg";
+import iconHoras from "../../assets/iconHoras.svg";
+import iconBook from "../../assets/iconBook.svg";
 
 const Main = () => {
   const [ dados, setDados ] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch('https://api.beta.unycos.com/u/courses/spotlights/natacion')
-      .then((r) => r.json())
-      .then((json) => setDados(json));
-      console.log(setDados)
-  }, []);
-
 
   return (
-    <section className={styles.Main}>
-       <div className={styles.contentMain}>
-          <img src={foto} alt="Foto" />
-            <div className={styles.contentMainTitle}> 
-              <h1> Mireia Belmonste </h1>
-              <h3> <span> Ensina </span> Natação </h3>
-            </div>
-       </div>
-    </section>
+      <section className={styles.Main}>
+
+        <div className={styles.contentMain}>
+            <img src={foto} alt="Foto" />
+              <div className={styles.contentMainTitle}> 
+                <h1> Mireia Belmonste </h1>
+                <h3> <span> Ensina </span> Natação </h3>
+              </div>
+        </div>
+
+        <div className={styles.contentInfo}>
+
+          <div className={styles.contentIcon}>
+            <img className={styles.iconCircle} src={iconEllipse} />
+            <img className={styles.iconHoras} src={iconHoras} /> <p> 12 MÒDULOS </p>
+          </div>
+
+          <div className={styles.contentIcon}>
+            <img className={styles.iconCircle1} src={iconEllipse} /> 
+            <img className={styles.iconBooks} src={iconBook} /> <p> +3,5 HORAS </p>
+          </div>
+
+          <div className={styles.contentButton}>
+            <button  className={styles.contentBtn}> Mais Informações </button>
+          </div>
+
+        </div>
+
+
+        <div className={styles.contentInfo2}>
+          <div className={styles.contentSplitInfo}>
+            <div className={styles.contentSplitLeft}> </div>
+          </div>
+        </div>
+
+      </section>
   );
 };
 
