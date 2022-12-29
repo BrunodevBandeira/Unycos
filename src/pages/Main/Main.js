0aa0664cfb50a8ac28fbea7ea8d8b2de1d913678
BrunodevBandeira <brunodev29ab@gmail.com>
2022-12-28 22:10:27 -0300

    WIP

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from "./Main.module.css";
 import foto from "../../assets/Hero.svg";
-import iconEllipse from "../../assets/iconEllipse.svg";
-import iconHoras from "../../assets/iconHoras.svg";
-import iconBook from "../../assets/iconBook.svg";
+import iconEllipse from "../../assets/icons/iconEllipse.svg";
+import iconHoras from "../../assets/icons/iconHoras.svg";
+import iconBook from "../../assets/icons/iconBook.svg";
+import image from "../../assets/image.svg";
+import natacao from "../../assets/natacao.svg";
+import natacao2 from "../../assets/natacao2.svg";
+import natacao3 from "../../assets/natacao3.svg";
+import iconE from "../../assets/icons/iconE.svg";
+import iconTest from "../../assets/icons/iconTest.svg";
+import iconTest2 from "../../assets/icons/iconTest2.svg";
+import iconVideo from "../../assets/icons/iconVideo.svg";
+import iconVideo2 from "../../assets/icons/iconVideo2.svg";
+import iconTarefa from "../../assets/icons/iconTarefa.svg";
 
 const Main = () => {
   const [ dados, setDados ] = React.useState([]);
@@ -41,7 +51,87 @@ const Main = () => {
 
         <div className={styles.contentInfo2}>
           <div className={styles.contentSplitInfo}>
-            <div className={styles.contentSplitLeft}> </div>
+            <div className={styles.contentSplitLeft}> 
+              <img src={image} />
+            </div>
+
+            <div className={styles.contentSplitRight}> 
+              <div className={styles.splitRightInfo}> 
+                  <h1> Treinar para o exito </h1>
+                  <div>
+                      E un fapt bine stabilit că cititorul va fi sustras de conţinutul citibil al unei pagini atunci când se uită la aşezarea în pagină. Scopul utilizării a Lorem Ipsum, este acela că are o distribuţie a literelor mai mult sau mai puţin normale
+                      literelor mai mult sau mai puţin normale...
+                  </div>
+              </div>
+
+              <div className={styles.contentSplitContainer}> 
+
+                <div className={styles.splitContainer}>
+                  <img src={natacao} />
+                  <p> Estilo de Natação </p>
+                </div>
+
+                <div className={styles.splitContainer}>
+                   <img src={natacao2} />
+                   <p> Preparação fisica </p>
+                </div>
+
+                <div className={styles.splitContainer}>
+                   <img src={natacao3} />
+                   <p> Condicionamento </p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.infoExclusive}>
+          <div className={styles.exclusiveContent}>
+            <h2> Conteudo exclusivo </h2>
+              <div className={styles.exclusiveContentIcons}>
+                    <div className={styles.contentIcons}>
+
+                        <div>
+                          <img className={styles.iconsE} src={iconE} /> 
+                          <p> E-Book </p>
+                        </div>
+
+                        <div>
+                          <img className={styles.iconsVideo} src={iconVideo} /> 
+                          <p> 360º Videos </p>
+                        </div>
+
+                        <div>
+                          <img className={styles.iconsTest} src={iconTest} /> 
+                          <p> Tests </p>
+                        </div>
+
+                        <div>
+                          <img className={styles.iconsTest2} src={iconTarefa} /> 
+                          <p> Tarefas </p>
+                        </div>
+                        
+                    </div>
+
+                    <div className={styles.exclusiveContentButton}>
+                      <button className={styles.contentBtn}>  Mais Informação </button>
+                    </div>
+              </div>
+
+
+          </div>
+        </div>
+
+
+
+        <div className={styles.lessonPlan}>
+          <h2> Planos de Ensino </h2>
+          <div className={styles.lessonPlanContent}>
+            <div className={styles.lessonPlanContent}>
+              
+            </div>
           </div>
         </div>
 
